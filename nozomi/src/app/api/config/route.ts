91795bcb2ddff367cdb6c4bfc1d867aa6108e070
natalie <22222885+espeon@users.noreply.github.com>
2024-08-29 13:58:14 -0500

    🩹: attempt to fix config api route

--- nozomi/src/app/api/config/route.ts
+++ nozomi/src/app/api/config/route.ts
@@ -1,9 +1,9 @@
 export async function GET() {
   const config = {
     // likely unneeded
-    internalMakiBaseURL: process.env.INTERNAL_MAKI_BASE_URL,
-    externalMakiBaseURL: process.env.EXTERNAL_MAKI_BASE_URL,
-    umiBaseURL: process.env.UMI_BASE_URL,
+    internalMakiBaseURL: process.env["INTERNAL_MAKI_BASE_URL"],
+    externalMakiBaseURL: process.env["EXTERNAL_MAKI_BASE_URL"],
+    umiBaseURL: process.env["UMI_BASE_URL"],
   };
   return new Response(JSON.stringify(config));
 }
